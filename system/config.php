c1336f26ed17acdebac5eb58f3b90005024a3e34
SuperDev <owen8064@gmail.com>
2022-12-19 11:06:41 -0600

    Update config.php

--- system/config.php
+++ system/config.php
@@ -6,7 +6,7 @@ define('app_desc','Secure and anonymous file sharing');
 define('FILELIST','jpeg,jpg,gif,png,zip,xls,doc,mp3,mp4,mpeg,wav,avi,rar,7z,txt');
 define('size_verification',true);
 define('file_destination','files');
-define('file_url_destination','https://dl.supers0ft.us/anonuptest');
+define('file_url_destination','https://anonuptest-web.hxqcwb.easypanel.host');
 define('max_size','10000000000');
 define('min_size','0');
 ?>
