a0f8328f82314af79e3f956303b025830008df7b
SuperDev <owen8064@gmail.com>
2022-12-19 11:06:08 -0600

    Add files via upload

---
+++ system/config.php
@@ -0,0 +1,12 @@
+<?php
+define('email','owen8064@gmail.com');
+define('password','12345678');
+define('app_name','AnonUpload - Secure File Sharing');
+define('app_desc','Secure and anonymous file sharing');
+define('FILELIST','jpeg,jpg,gif,png,zip,xls,doc,mp3,mp4,mpeg,wav,avi,rar,7z,txt');
+define('size_verification',true);
+define('file_destination','files');
+define('file_url_destination','https://dl.supers0ft.us/anonuptest');
+define('max_size','10000000000');
+define('min_size','0');
+?>
