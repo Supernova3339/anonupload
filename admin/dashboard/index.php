6e0cf126ff5d88273ca4f4298b2555a6c20c610d
SuperDev <owen8064@gmail.com>
2022-12-21 11:37:53 -0600

    Update index.php

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -107,15 +107,15 @@ $plugin_count = count(glob('../../plugins/*', GLOB_ONLYDIR));
     </div>
 </div>
   <br>
-<?php if(!plausibledatadomain&plausibledomain&plausibleembedtoken == ''){ ?>
+<?php if(!plausibledatadomain&plausibledomain&plausibleembedtoken): { ?>
 <div class="alert warning">
   <span class="closebtn">&times;</span>  
   <strong>Warning!</strong>&nbsp;Plausible analytics have not been configured.
 </div>
-<?php }else{ ?>
+<?php else: ?>
 <iframe plausible-embed src="<?=plausibledomain?>/share/<?=plausibledatadomain?>?auth=<?=plausibleembedtoken?>&embed=true&theme=light&background=%23EBECED" scrolling="no" frameborder="0" loading="lazy" style="width: 1px; min-width: 100%; height: 1600px;"></iframe>
 <script async src="<?=plausibledomain?>js/embed.host.js"></script>
-<?php } ?>
+<?php endif; ?>
 
  
 
