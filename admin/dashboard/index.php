b73d46b53abc6e19d1d2b092c39e4229a69f0c16
SuperDev <owen8064@gmail.com>
2022-12-21 11:47:32 -0600

    Update index.php

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -107,15 +107,19 @@ $plugin_count = count(glob('../../plugins/*', GLOB_ONLYDIR));
     </div>
 </div>
   <br>
-<?php if(!plausibledatadomain&plausibledomain&plausibleembedtoken): ?>
+<?php if(!plausibledatadomain&plausibledomain&plausibleembedtoken){
+  echo '
 <div class="alert warning">
   <span class="closebtn">&times;</span>  
   <strong>Warning!</strong>&nbsp;Plausible analytics have not been configured.
 </div>
-<?php if(plausibledatadomain&plausibledomain&plausibleembedtoken): ?>
-<iframe plausible-embed src="<?=plausibledomain?>/share/<?=plausibledatadomain?>?auth=<?=plausibleembedtoken?>&embed=true&theme=light&background=%23EBECED" scrolling="no" frameborder="0" loading="lazy" style="width: 1px; min-width: 100%; height: 1600px;"></iframe>
-<script async src="<?=plausibledomain?>js/embed.host.js"></script>
-<?php endif; ?>
+  }';
+  
+if(plausibledatadomain&plausibledomain&plausibleembedtoken){
+  echo '
+<iframe plausible-embed src="' . plausibledomain . '/share/' . plausibledatadomain . '?auth= ' . plausibleembedtoken . '&embed=true&theme=light&background=%23EBECED" scrolling="no" frameborder="0" loading="lazy" style="width: 1px; min-width: 100%; height: 1600px;"></iframe>
+<script async src="' . plausibledomain . 'js/embed.host.js"></script> ';
+}
 
  
 
