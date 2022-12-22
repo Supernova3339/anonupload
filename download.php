e6b76497dc2ca7571a5dad6f8bf9471db15cc73d
SuperDev <owen8064@gmail.com>
2022-12-22 13:25:06 -0600

    Update download.php

--- download.php
+++ download.php
@@ -40,7 +40,7 @@ $core = new Core();
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?=app_title?> - Download</title>
+	<title><?=app_name?> - Download</title>
 	<link rel="stylesheet" type="text/css" href="assets/main.css">
 </head>
 <body>
