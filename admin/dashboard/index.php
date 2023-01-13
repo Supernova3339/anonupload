0a29401e414539684982e3594608aaffbb08289e
SuperDev <supernova@supers0ft.us>
2023-01-13 11:06:04 -0600

    Update index.php

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -67,7 +67,8 @@ return $bytes . ' B';
 $plugin_count = count(glob('../../plugins/*', GLOB_ONLYDIR));
 
 // Get version
-		$releases = json_decode(file_get_contents(github_api_url));
+		$verurl = github_api_url;
+		$releases = json_decode(file_get_contents($verurl));
 		
 		$server_version = $releases['tag_name'];
 
